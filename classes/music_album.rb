6d0ef7805f5710107ae810f24ba411e6274c374e
Hyder <hyderali400@gmail.com>
2023-06-06 15:31:20 +0530

    add music_album and genre classes to code base

---
+++ classes/music_album.rb
@@ -0,0 +1,17 @@
+require_relative 'item'
+require_relative 'genre'
+
+
+class MusicAlbum < Item
+  attr_reader :on_spotify, :publish_date
+
+
+  def initialize(on_spotify, publish_date)
+    super(publish_date)
+    @on_spotify = on_spotify
+  end
+
+  def can_be_archived?
+    super && on_spotify == true
+  end
+end
