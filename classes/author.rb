82ad2265907ed29304d18944b485a4eaf80c86e8
Kagheni <Kaghenimbale@gmail.com>
2023-06-06 10:20:37 +0200

    Solve the linters

--- classes/author.rb
+++ classes/author.rb
@@ -1,14 +1,15 @@
-class author
-    attr_accessor :first_name, :last_name, :items
-    def initialize(first_name, last_name)
-        @id = Random.rand(1..1000)
-        @first_name = first_name
-        @last_name = last_name
-        @items = []
-    end
+class Author
+  attr_accessor :first_name, :last_name, :items
 
-    def Add_item(item)
-        @items << item
-        item.author = self
-    end
+  def initialize(first_name, last_name)
+    @id = Random.rand(1..1000)
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+    item.author = self
+  end
 end
--- classes/game.rb
+++ classes/game.rb
@@ -1,13 +1,13 @@
 require_relative './item'
 
 class Game < Item
-    def initialize(publish_date, multiplayer, last_played_at)
-        super(publish_date)
-        @multiplayer = multiplayer
-        @last_played_at = last_played_at
-    end
+  def initialize(publish_date, multiplayer, last_played_at)
+    super(publish_date)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+  end
 
-    def can_be_archived?()
-        super && Time.now.year - Date.parse(@last_played).year > 2
-    end
+  def can_be_archived?()
+    super && Time.now.year - Date.parse(@last_played).year > 2
+  end
 end
