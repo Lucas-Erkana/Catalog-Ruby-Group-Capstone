167b39291585217674330bac8775f38ca8803328
Kagheni <Kaghenimbale@gmail.com>
2023-06-06 07:51:49 +0200

    Add instance of game and author class

--- classes/author.rb
+++ classes/author.rb
@@ -1,2 +1,8 @@
 class author
+    def initialize(first_name, last_name)
+        @id = Random.rand(1..1000)
+        @first_name = first_name
+        @last_name = last_name
+        @items = []
+    end
 end
--- classes/game.rb
+++ classes/game.rb
@@ -1,2 +1,6 @@
 class game
+    def initialize(multiplayer, last_played_at)
+        @multiplayer = multiplayer
+        @last_played_at = last_played_at
+    end
 end
