73d6a78bafba55ddbdd9ae0f762e01b2f691d524
Kagheni <Kaghenimbale@gmail.com>
2023-06-06 08:00:13 +0200

    Add the author method add_item

--- classes/author.rb
+++ classes/author.rb
@@ -1,8 +1,14 @@
 class author
+    attr_accessor :first_name, :last_name, :items
     def initialize(first_name, last_name)
         @id = Random.rand(1..1000)
         @first_name = first_name
         @last_name = last_name
         @items = []
+    end
+
+    def Add_item(item)
+        @items << item
+        item.author = self
     end
 end
