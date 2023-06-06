760a5a6d73359c9c85356f4d52fd56ef5f1c33fc
Kagheni <Kaghenimbale@gmail.com>
2023-06-06 08:33:13 +0200

    Add can be achrived methods in the Item class

--- classes/game.rb
+++ classes/game.rb
@@ -1,6 +1,13 @@
-class game
-    def initialize(multiplayer, last_played_at)
+require_relative './item'
+
+class game < Item
+    def initialize(publish_date, multiplayer, last_played_at)
+        super(publish_date)
         @multiplayer = multiplayer
         @last_played_at = last_played_at
+    end
+
+    def can_be_archived?()
+        super && Time.now.year - Date.parse(@last_played).year > 2
     end
 end
