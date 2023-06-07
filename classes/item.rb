ad2e63f3e41dff753f976b212831efbb8237cec1
Lucas David Erkana <lderkana21x@gmail.com>
2023-06-07 14:12:07 +0200

    remove source from item Class

--- classes/item.rb
+++ classes/item.rb
@@ -1,7 +1,7 @@
 require 'date'
 
 class Item
-  attr_reader :id, :genre, :author, :source, :label
+  attr_reader :id, :genre, :author, :label
   attr_accessor :publish_date
 
   def initialize(publish_date)
