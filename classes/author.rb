559f8773019d8cfd21c4221cb80f2dbd22c30f1d
Kagheni <Kaghenimbale@gmail.com>
2023-06-08 08:04:31 +0200

    Add changes in game and author files for the test

--- classes/author.rb
+++ classes/author.rb
@@ -12,4 +12,12 @@ class Author
     @items << item
     item.author = self
   end
+
+  def to_hash
+    {
+      id: @id,
+      first_name: @first_name,
+      last_name: @last_name
+    }
+  end
 end
